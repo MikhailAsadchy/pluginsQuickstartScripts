af10cbfaad454a9d67a57fd04fd0a29973275e00
mikhial_asadchy <mikhialasadchy@gmail.com>
2018-06-16 00:14:54 +0300

    bitrix

--- common/php/install/setupPhp.sh
+++ common/php/install/setupPhp.sh
@@ -9,6 +9,7 @@ sudo add-apt-repository ppa:ondrej/apache2
 sudo apt-get update
 sudo apt-get install php${phpVersion}-cli php${phpVersion}-common libapache2-mod-php${phpVersion} php${phpVersion} php${phpVersion}-mysql php${phpVersion}-fpm php${phpVersion}-curl php${phpVersion}-gd php${phpVersion}-mysql php${phpVersion}-bz2
 sudo apt-get install php${phpVersion}-mbstring php${phpVersion}-zip php${phpVersion}-xml
+sudo apt-get install php-xdebug
 
 echo --------- install dependencies --------
 sudo apt-get install libapache2-mod-php${phpVersion} php${phpVersion}-mysql php${phpVersion}-curl php${phpVersion}-json
